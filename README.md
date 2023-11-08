3eafe73e2c9dda856b9cc27ad5e1eebf7fd09a7d
Maki <maki@hotmilk.space>
2023-11-07 22:34:07 -0500

    Add graalvm-ee

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Maki's Pterodactyl Yolks
 
 -   `ghcr.io/makidoll/pterodactyl-yolks:java-17-graalvm`
+-   `ghcr.io/makidoll/pterodactyl-yolks:java-17-graalvm-ee`
--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 docker build -t ghcr.io/makidoll/pterodactyl-yolks:java-17-graalvm -f java-17-graalvm.Dockerfile .
+docker build -t ghcr.io/makidoll/pterodactyl-yolks:java-17-graalvm-ee -f java-17-graalvm-ee.Dockerfile .
