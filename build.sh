a322a4cd78b9a0949e5feb448abe05d75774465b
Jenzoga <tchaovaluksakul@gmail.com>
2025-04-23 03:20:29 +0700

    correct java build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 docker build -t ghcr.io/jenzoga/pterodactyl-yolks:java-17-graalvm-ee -f java-17-graalvm-ee.Dockerfile .
-docker build -t ghcr.io/jenzoga/pterodactyl-yolks:java-21-graalvm-ee -f java-21-graalvm-ee.Dockerfile .
+docker build -t ghcr.io/jenzoga/pterodactyl-yolks:java-21-graalvm -f java-21-graalvm-ee.Dockerfile .
 docker build -t ghcr.io/jenzoga/pterodactyl-yolks:java-8-graalvm-ee -f java-8-graalvm-ee.Dockerfile .
-docker build -t ghcr.io/jenzoga/pterodactyl-yolks:java-22-graalvm-ee -f java-22-graalvm-ee.Dockerfile .
 
 
