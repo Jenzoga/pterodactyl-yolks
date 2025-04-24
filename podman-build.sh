c48e529022a3e3e3a7340505031fa873bb4d222b
Jenzoga <tchaovaluksakul@gmail.com>
2025-04-24 21:48:10 +0700

    added java 22

--- podman-build.sh
+++ podman-build.sh
@@ -2,3 +2,4 @@ podman build --platform=linux/amd64 -t ghcr.io/jenzoga/pterodactyl-yolks:java-17
 podman build --platform=linux/amd64 -t ghcr.io/jenzoga/pterodactyl-yolks:java-8-graalvm-ee -f java-8-graalvm-ee.Dockerfile .
 podman build --platform=linux/amd64 -t ghcr.io/jenzoga/pterodactyl-yolks:java-17-graalvm -f java-17-graalvm.Dockerfile .
 podman build --platform=linux/amd64 -t ghcr.io/jenzoga/pterodactyl-yolks:java-21-graalvm -f java-21-graalvm.Dockerfile .
+podman build --platform=linux/amd64 -t ghcr.io/jenzoga/pterodactyl-yolks:java-22-graalvm -f java-22-graalvm.Dockerfile .
